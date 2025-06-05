0f56c9433600f67e1729b00ad2d353f4d0f55621
Andres Silva <andres.silva41202@gmail.com>
2025-06-05 16:59:38 -0300

    add endpoints and request handlers for product CRUD

--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -8,5 +8,50 @@ router.get('/products', async(req, res) => {
     return res.json(products)
 })
 
+router.post('/products', async(req, res) => {
+    const {code, name, description, price, brand} = req.body
+    const newProduct = await prisma.products.create({
+        data: {
+            code: code,
+            name: name,
+            description: description,
+            price: price,
+            brand: brand
+        }
+    })
+    return res.json(newProduct)
+})
+
+router.get('/products/:id', async(req, res) => {
+    const id = req.params.id
+    const product = await prisma.products.findUnique({
+        where: {
+            id: id
+        }
+    })
+    return res.json(product)
+})
+
+router.delete('/products/:id', async(req, res) => {
+    const id = req.params.id
+    const productDeleted = await prisma.products.delete({
+        where: {
+            id: id
+        }
+    })
+    return res.json(productDeleted)
+})
+
+router.put('/products/:id', async(req, res)=> {
+    const id = req.params.id
+    const productUpdated = await prisma.products.update({
+        where: {
+            id: id
+        },
+        data: req.body
+    })
+    return res.json(productUpdated)
+})
+
 
 export default router
