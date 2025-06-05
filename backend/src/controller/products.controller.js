74c946f044d1780e6763ca4dd78e0bc0d1d6428f
Andres Silva <andres.silva41202@gmail.com>
2025-06-05 17:20:32 -0300

    separate product endpoints into controller

---
+++ backend/src/controller/products.controller.js
@@ -0,0 +1,81 @@
+import prisma from "../db.js";
+
+export const createProduct = async(req, res) => {
+    try{
+        const {code, name, description, price, brand} = req.body
+        const newProduct = await prisma.products.create({
+            data: {
+                code: code,
+                name: name,
+                description: description,
+                price: price,
+                brand: brand
+            }
+        })
+        return res.json(newProduct)
+    }
+    catch (error){
+        console.log(error)
+        return res.status(500).json({error: "Error interno al crear el producto"})
+    }
+}
+
+export const getAllProducts = async(req, res) => {
+    try{
+        const products = await prisma.products.findMany()
+        return res.json(products)
+    }
+    catch (error){
+        console.log(error)
+        return res.status(500).json({error: "Error interno al traer los productos"})
+    }
+}
+
+export const getOneProduct = async(req, res) => {
+    try {
+        const id = req.params.id
+        const product = await prisma.products.findUnique({
+            where: {
+                id: id
+            }
+        })
+        return res.json(product)
+    }
+    catch (error) {
+        console.log(error)
+        return res.status(500).json({error: "Error interno al traer el producto"})
+    }
+}
+
+export const deleteProduct = async(req, res) => {
+    try {
+        const id = req.params.id
+        const productDeleted = await prisma.products.delete({
+            where: {
+                id: id
+            }
+        })
+        return res.json(productDeleted)
+    }
+    catch (error) {
+        console.log(error)
+        return res.status(500).json({error: "Error interno al eliminar el producto"})
+    }
+}
+
+export const updateProduct = async(req, res) => {
+    try {
+        const id = req.params.id
+        const productUpdated = await prisma.products.update({
+            where: {
+                id: id
+            },
+            data: req.body
+        })
+        return res.json(productUpdated)
+    }
+    catch (error) {
+        console.log(error)
+        return res.status(500).json({error: "Error interno al actualizar el producto"})   
+    }
+}
--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -1,57 +1,16 @@
 import { Router } from "express";
-import prisma from "../db.js";
+import { createProduct, deleteProduct, getAllProducts, getOneProduct, updateProduct } from "../controller/products.controller.js";
 
 const router = Router()
 
-router.get('/products', async(req, res) => {
-    const products = await prisma.products.findMany()
-    return res.json(products)
-})
+router.get('/products', getAllProducts)
 
-router.post('/products', async(req, res) => {
-    const {code, name, description, price, brand} = req.body
-    const newProduct = await prisma.products.create({
-        data: {
-            code: code,
-            name: name,
-            description: description,
-            price: price,
-            brand: brand
-        }
-    })
-    return res.json(newProduct)
-})
+router.post('/products', createProduct)
 
-router.get('/products/:id', async(req, res) => {
-    const id = req.params.id
-    const product = await prisma.products.findUnique({
-        where: {
-            id: id
-        }
-    })
-    return res.json(product)
-})
+router.get('/products/:id', getOneProduct)
 
-router.delete('/products/:id', async(req, res) => {
-    const id = req.params.id
-    const productDeleted = await prisma.products.delete({
-        where: {
-            id: id
-        }
-    })
-    return res.json(productDeleted)
-})
+router.delete('/products/:id', deleteProduct)
 
-router.put('/products/:id', async(req, res)=> {
-    const id = req.params.id
-    const productUpdated = await prisma.products.update({
-        where: {
-            id: id
-        },
-        data: req.body
-    })
-    return res.json(productUpdated)
-})
-
+router.put('/products/:id', updateProduct)
 
 export default router
