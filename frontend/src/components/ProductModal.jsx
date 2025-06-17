787990e03528e952232563eb6d94e7de43826bea
Andres Silva <andres.silva41202@gmail.com>
2025-06-17 17:31:33 -0300

    extract ProductForm into reusable component

--- frontend/src/components/ProductModal.jsx
+++ frontend/src/components/ProductModal.jsx
@@ -1,56 +1,19 @@
-import { zodResolver } from '@hookform/resolvers/zod'
-import {useForm} from 'react-hook-form'
 import { updateProductSchema } from '../../../validation/productSchema.js'
-import { useEffect} from 'react'
 import { useProducts } from '../hooks/useProducts.js'
+import ProductForm from './ProductForm.jsx'
 
 function ProductModal({productUpdate, onClose, onUpdate}) {
     const {updateProduct} = useProducts()
-    const {register, handleSubmit, reset, formState: {errors}} = useForm({
-        mode: 'onChange',
-        resolver: zodResolver(updateProductSchema)
-    })
 
-    useEffect(()=> {
-        if (productUpdate) reset(productUpdate)
-    }, [productUpdate, reset])
-
-    const onSubmit = handleSubmit((data) => {
+    const onSubmit = (data) => {
         updateProduct(data, productUpdate)
         onUpdate()
-    })
-
+    }
 
     return productUpdate ? (
         <div className='flex flex-col h-screen w-screen fixed top-0 left-0' style={{backgroundColor: 'rgb(0,0,0,0.6)'}}>
             <button onClick={onClose}>X</button>
-            <form onSubmit={onSubmit}>
-                <div>
-                    <label htmlFor='name'>Nombre</label>
-                    <input type="text" autoComplete="name" id= "name"  {...register("name")} placeholder={productUpdate.name}/>
-                    {errors.name && <span className="text-red-600">{errors.name.message}</span>}
-                </div>
-                <div>
-                    <label htmlFor='brand'>Marca</label>
-                    <input type="text" autoComplete="brand" id= "brand" {...register("brand")}/>
-                    {errors.brand && <span className="text-red-600">{errors.brand.message}</span>}
-                </div>
-                <div>
-                    <label htmlFor='purchasePrice'>precio de compra</label>
-                    <input type="number" autoComplete="purchasePrice" id= "purchasePrice" {...register("purchasePrice")}/>
-                    {errors.purchasePrice && <span className="text-red-600">{errors.purchasePrice.message}</span>}
-                </div>
-                <div>
-                    <label htmlFor='salePrice'>precio de venta</label>
-                    <input type="number" autoComplete="salePrice" id= "salePrice" {...register("salePrice")}/>
-                    {errors.salePrice && <span className="text-red-600">{errors.salePrice.message}</span>}
-                </div>
-                <div>
-                    <label htmlFor='description'>Descripcion (opcional)</label>
-                    <input type="text" autoComplete="description" id= "description" {...register("description")}/>
-                </div>
-                <button type='submit'>Actualizar</button>
-            </form>
+            <ProductForm schema={updateProductSchema} action="Actualizar" onSubmit={onSubmit} defaultValues={productUpdate}/>
         </div>
     ): (<></>)
 }
--- frontend/src/pages/FormProductsPage.jsx
+++ frontend/src/pages/FormProductsPage.jsx
@@ -1,19 +1,14 @@
-import { zodResolver } from '@hookform/resolvers/zod'
-import {useForm} from 'react-hook-form'
 import { productSchema } from '../../../validation/productSchema.js'
 import { useEffect, useState } from 'react'
 import {ToastContainer} from 'react-toastify'
 import { notify } from '../utils/notifyToast.js'
-import ProductModal from '../components/ProductModal.jsx'
 import { useProducts } from '../hooks/useProducts.js'
+import ProductModal from '../components/ProductModal.jsx'
+import ProductForm from '../components/ProductForm.jsx'
 
 function FormProductsPage() {
     const [productUpdate, setProductUpdate] = useState(null)
     const {createProduct, deleteProduct, fetchProducts, products} = useProducts()
-    const {register, handleSubmit, formState: {errors}} = useForm({
-        mode: 'onChange',
-        resolver: zodResolver(productSchema)
-    })
 
     useEffect(() => {
         if (productUpdate == null){
@@ -25,54 +20,14 @@ function FormProductsPage() {
         createProduct(formProduct)
     }
 
-    const onInvalid= () => {
-        notify('error','Por favor ingrese todos los datos')
-    }
-
     const onUpdate= () => {
         setProductUpdate(null)
         notify('success', 'Producto actualizado correctamente!')
     }
 
-
     return (
         <>
-            <form onSubmit={handleSubmit(onValid, onInvalid)} className= "flex flex-col pt-20 m-10 gap-2">
-                <div className="flex justify-between">
-                    <label htmlFor="name">Nombre</label>
-                    <div className="flex gap-4">
-                        {errors.name && <span className="text-red-600">{errors.name.message}</span>}
-                        <input type="text" autoComplete="name" id= "name" {...register("name")}/>
-                    </div>
-                </div>
-                <div className="flex justify-between">
-                    <label htmlFor="purchasePrice">Precio de compra</label>
-                    <div className="flex gap-4">
-                        {errors.purchasePrice && <span className="text-red-600">{errors.purchasePrice.message}</span>}
-                        <input type="number" autoComplete="purchasePrice" id="purchasePrice" {...register("purchasePrice")}/>
-                    </div>
-                </div>
-                <div className="flex justify-between">
-                    <label htmlFor="salePrice">Precio de venta</label>
-                    <div className="flex gap-4">
-                        {errors.salePrice && <span className="text-red-600">{errors.salePrice.message}</span>}
-                        <input type="number" autoComplete="salePrice" id="salePrice" {...register("salePrice")}/>
-                    </div>
-                </div>
-                <div className="flex justify-between">
-                    <label htmlFor="brand">Marca</label>
-                    <div className="flex gap-4">
-                        {errors.brand && <span className="text-red-600">{errors.brand.message}</span>}
-                        <input type="text" autoComplete="brand" id="brand" {...register("brand")}/>
-                    </div>
-                </div>
-                <div className="flex justify-between">
-                    <label htmlFor="description">Descripcion (Opcional)</label>
-                    <input type="text" autoComplete="description" id="description" {...register("description")}/>
-                </div>
-                
-                <button type="submit">Send!</button>
-            </form>
+            <ProductForm action="Crear" schema={productSchema} onSubmit={onValid}/>
 
             <div className="m-10 bg-neutral-900 p-5">
                 {products.map((product)=> (
