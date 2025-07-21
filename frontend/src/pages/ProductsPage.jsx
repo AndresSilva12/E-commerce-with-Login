8ceee9fbb54ce94efd8eacb1344cdafb644957cf
Andres Silva <andres.silva41202@gmail.com>
2025-07-21 20:42:23 -0300

    chore: install and configure Chakra UI

--- frontend/src/pages/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage.jsx
@@ -3,6 +3,7 @@ import { ToastContainer } from "react-toastify";
 import { useProducts } from "../context/ProductContext";
 import ProductModal from "../components/ProductModal";
 import { deleteAlert } from "../utils/alerts";
+import { Button, Card, Image, Text, Grid, Accordion, Span, HStack, Badge, Avatar } from "@chakra-ui/react"
 
 function ProductsPage() {
     const [modal, setModal] = useState(false)
@@ -30,34 +31,45 @@ function ProductsPage() {
 
     return (
         <>
-            <div className="m-10 bg-neutral-900 p-5">
-                {products.map((product) => (
-                    <div key={product.id} >
-                        <div className="flex justify-between items-center mb-5">
-                            <div className="flex justify-between items-center gap-5">
-                                <p>Nombre:<br />{product.name}</p>
-                                <p>Marca:<br />{product.brand}</p>
-                            </div>
-                            <div className="flex justify-between items-center gap-5">
-                                <p>Precio de compra:<br />${new Intl.NumberFormat("es-AR").format(product.purchasePrice)}</p>
-                                <p>Precio de venta:<br />${new Intl.NumberFormat("es-AR").format(product.salePrice)}</p>
-                            </div>
-                            <button className="bg-green-900" onClick={() => { handleUpdate(product) }}>Editar</button>
-                            <button className="bg-red-900" onClick={() => { handleDelete(product.id) }}>Eliminar</button>
-                        </div>
 
-                        {product.variants && product.variants.map((variant) => (
-                            <div key={variant.id} className="flex justify-center gap-3 bg-neutral-700 m-1">
-                                <p>Code: {variant.code}</p>
-                                <p>Size: {variant.size}</p>
-                                <p>Color: {variant.color}</p>
-                                <p>Stock: {variant.stock}</p>
-                                {variant.image && <img src={variant.image} className="w-20 h-20 object-cover" />}
-                            </div>
-                        ))}
-                    </div>
-                ))}
-            </div>
+            {products.map((product) => (
+                <Accordion.Root collapsible key={product.id}>
+                    <Accordion.Item >
+                        <Accordion.ItemTrigger>
+                            <Avatar.Root shape="rounded">
+                                <Avatar.Image src={product.variants[0].image} />
+                                <Avatar.Fallback name={product.name} />
+                            </Avatar.Root>
+                            <Span flex="1">{product.name} {product.brand}</Span>
+                            <Accordion.ItemIndicator />
+                        </Accordion.ItemTrigger>
+                        <Accordion.ItemContent>
+                            <Grid templateColumns="repeat(3, 1fr)" gap="6" >
+                                {product.variants && product.variants.map((variant) => (
+
+                                    <Card.Root maxW="sm" size="sm" overflow="hidden" key={variant.id} >
+                                        <Image src={variant.image} h="auto" w="full" fit="contain" />
+                                        <Card.Body gap="2">
+                                            <Card.Title>{product.name} {product.brand}</Card.Title>
+                                            <Card.Description>{product.description}</Card.Description>
+                                            <HStack mt="1">
+                                                <Badge>Talle {variant.size}</Badge>
+                                                <Badge>{variant.color}</Badge>
+                                            </HStack>
+                                            <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">${new Intl.NumberFormat("es-AR").format(product.purchasePrice)}</Text>
+                                        </Card.Body>
+                                        <Card.Footer gap="2">
+                                            <Button variant="solid" onClick={() => { handleUpdate(product) }}>Editar</Button>
+                                            <Button variant="ghost" onClick={() => { handleDelete(product.id) }}>Eliminar</Button>
+                                        </Card.Footer>
+                                    </Card.Root>
+                                ))}
+                            </Grid>
+                        </Accordion.ItemContent>
+                    </Accordion.Item>
+                </Accordion.Root>
+            ))}
+
             <button className="fixed bottom-0 right-0 p-2 bg-blue-600 rounded-full" onClick={handleCreate}>+</button>
             {modal && (<ProductModal productUpdate={productUpdate} onClose={() => { setModal(false) }} onSubmit={() => { onSubmit() }} />)}
 
