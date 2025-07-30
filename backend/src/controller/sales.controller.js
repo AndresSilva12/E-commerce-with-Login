f7c0a5fb090dd21465680698bf6699dda4922473
Andres Silva <andres.silva41202@gmail.com>
2025-07-30 12:34:28 -0300

    feature: validate variant existence before deleting stockEntry or sale, and ensure atomic operations with Prisma transactions

--- backend/src/controller/sales.controller.js
+++ backend/src/controller/sales.controller.js
@@ -1,3 +1,4 @@
+import { id } from "zod/v4/locales"
 import prisma from "../db.js"
 export const createNewSale = async(req, res) => {
     try {
@@ -56,13 +57,46 @@ export const getAllSales = async(req, res) => {
 
 export const deleteSale = async(req, res) => {
     try {
-        const idSelected = req.params.id
-        const saleDeleted = await prisma.sales.delete({
-            where: {
-                id: idSelected
+        const result = await prisma.$transaction( async(tx) => {
+            const idSelected = req.params.id
+            const saleExist = await tx.sales.findFirst({where: {id: idSelected}, include: {items: true}})
+            if (!saleExist) throw new Error(`Venta inexistente: ${idSelected}`)
+
+            const variantsThatNotExist = {}
+            for (const item of saleExist.items){
+                const variantExist = await tx.productVariant.findFirst({where: {id: item.variantId}})
+                if (!variantExist) {
+                    variantsThatNotExist[item.variantId] = `variante inexistente: ${item.variantId}`
+                    continue
+                }
             }
+            if (Object.keys(variantsThatNotExist).length > 0) throw new Error(JSON.stringify(variantsThatNotExist))
+
+            const saleDeleted = await tx.sales.delete({
+                where: {
+                    id: idSelected
+                },
+                include: {
+                    items: true
+                }
+            })
+
+            const variantsUpdates = {}
+            for (const item of saleDeleted.items){
+                const variantReverted = await tx.productVariant.update({
+                    where: {id: item.variantId},
+                    data: {
+                        stock: {
+                            increment: item.quantity
+                        }
+                    }
+                })
+                variantsUpdates[variantReverted.code] = variantReverted
+            }
+
+            return {saleDeleted, variantsUpdates}
         })
-        return res.json(saleDeleted)
+        return res.json({saleDeleted: result.saleDeleted, variantsUpdates: result.variantsUpdates})
     } catch (error) {
         console.log(error);
         return res.status(500).json({ error: "Error interno durante el proceso" });
--- backend/src/controller/stockEntries.controller.js
+++ backend/src/controller/stockEntries.controller.js
@@ -1,3 +1,4 @@
+import { id } from "zod/v4/locales"
 import prisma from "../db.js"
 
 export const createStockEntry = async(req, res) => {
@@ -61,13 +62,47 @@ export const getAllStockEntries = async(req, res) => {
 
 export const deleteEntry = async(req, res) =>{
     try {
-        const idSelected = req.params.id
-        const entryDeleted = await prisma.stockEntry.delete({
-            where: {
-                id: idSelected
+        const result = await prisma.$transaction( async(tx) => {
+            const idSelected = req.params.id
+            const entryExist = await tx.stockEntry.findFirst({where: {id: idSelected}, include: {items: true}})
+            if (!entryExist) {throw new Error(`Entrada no existente: ${idSelected}`)}
+
+            const variantsThatNotExist = {}
+            const variantsWithLowStock = {}
+            for (const item of entryExist.items){
+                const variantExist = await tx.productVariant.findFirst({where: {id: item.variantId}})
+                if (!variantExist){
+                    variantsThatNotExist[item.variantId] = `Variante inexistente: ${item.variantId}`
+                    continue
+                }
+                if (variantExist.stock < item.quantity){
+                    variantsWithLowStock[item.variantId] = `Error, stock de ${variantExist.code} por debajo de la cantidad ${item.quantity}`
+                }
             }
+            if (Object.keys(variantsThatNotExist).length > 0) throw new Error(JSON.stringify(variantsThatNotExist))
+            if (Object.keys(variantsWithLowStock).length > 0) throw new Error(JSON.stringify(variantsWithLowStock))
+
+            const entryDeleted = await tx.stockEntry.delete({
+                where: {
+                    id: idSelected
+                },
+                include: {
+                    items: true
+                }
+            })
+
+            const variantsUpdates = {}
+            for (const item of entryDeleted.items){
+                const variantReverted = await tx.productVariant.update({
+                    where: {id: item.variantId},
+                    data: {stock: {decrement: item.quantity}}
+                })
+                variantsUpdates[variantReverted.code] = variantReverted
+            }
+
+            return {entryDeleted, variantsUpdates}
         })
-        return res.json(entryDeleted)
+        return res.json({entryDeleted: result.entryDeleted, variantsUpdates: result.variantsUpdates})
     } catch (error) {
         console.log(error);
         return res.status(500).json({ error: "Error interno durante el proceso" });
