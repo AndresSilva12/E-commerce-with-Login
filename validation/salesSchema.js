278c11cc8aca23bd01e5bf94c7f1d20195d8de8c
Andres Silva <andres.silva41202@gmail.com>
2025-07-30 17:21:34 -0300

    fix: update Zod schemas for sales and stock entries validation

--- validation/salesSchema.js
+++ validation/salesSchema.js
@@ -1,23 +1,28 @@
 import {z} from 'zod'
 
 export const saleSchema = z.object({
-    userId: z.coerce.number()
+    userId: z.number({
+        required_error: "El id de Usuario es obligatorio",
+        invalid_type_error: "Formato no valido. Debe ser un Numero",
+    })
     .int("Debe ingresar un número entero")
     .gte(1, "Debe ser un número positivo")
     ,
-    totalPrice: z.coerce.number()
+    totalPrice: z.coerce.number({
+        required_error: "El id de Usuario es obligatorio",
+        invalid_type_error: "Formato no valido. Debe ser un Numero",
+    })
     .gte(1, "Debe ser un numero mayor a 0")
     ,
     items: z.array(z.object({
             variantId: z.string()
                 .cuid("El id no es válido")
                 .min(1, "El id de la variante es obligatorio"),
-            quantity: z.coerce.number({
-                required_error: "La cantidad es obligatorio",
-                invalid_type_error: "Formato no valido",
-            })
-                .int("Debe ingresar un numero entero")
-                .gte(1, "Debe ser un numero mayor a 0"),
+            quantity: z.preprocess((val) => Number(val),
+                z.number({ invalid_type_error: "Debe ser un número" })
+                .int("Debe ser un número entero")
+                .gte(1, "Debe ser mayor a 0")
+            ),
         })
     ).min(1, "Debe tener al menos un item de venta")
 })
--- validation/stockEntriesSchema.js
+++ validation/stockEntriesSchema.js
@@ -1,15 +1,20 @@
 import { z } from "zod";
 
 export const stockEntriesSchema = z.object({
-    userId: z.coerce.number()
+    userId: z.number({
+        required_error: "El id de Usuario es obligatorio",
+        invalid_type_error: "Formato no valido",
+    })
         .int("Debe ser un numero entero")
         .gte(1, "Debe ser un numero positivo"),
     items: z.array(z.object({
         variantId: z.string()
             .cuid("El id no es válido")
             .min(1, "El Id de la variante es obligatorio"),
-        quantity: z.coerce.number()
-            .int("Debe ser un numero entero")
-            .gte(1, "Debe ser un numero mayor a 0"),
+        quantity: z.preprocess((val) => Number(val),
+            z.number({ invalid_type_error: "Debe ser un número" })
+            .int("Debe ser un número entero")
+            .gte(1, "Debe ser mayor a 0")
+        ),
     })).min(1, "Debe tener al menos un item de entrada")
 })
