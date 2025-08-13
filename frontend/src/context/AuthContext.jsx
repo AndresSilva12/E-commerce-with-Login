08c1027389c979b97001938d64297b53d2c75afd
Andres Silva <andres.silva41202@gmail.com>
2025-08-13 17:53:06 -0300

    feature: implement periodic refresh token request every 10 minutes in AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,31 +2,46 @@ import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext()
 
-export const AuthProvider = ({children}) => {
+export const AuthProvider = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(null)
 
-    useEffect(()=> {
-        fetch('http://localhost:3000/api/dashboard',{
+    useEffect(() => {
+        const intervalId = setInterval(async () => {
+            const res = await fetch('http://localhost:3000/api/refresh-token', {
+                method: 'POST',
+                credentials: "include",
+                headers: {
+                    "content-type": "application/json"
+                }
+            })
+            const data = await res.json()
+            console.log(data)
+        }, 600000);
+        return () => clearInterval(intervalId)
+    }, [])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/dashboard', {
             credentials: 'include'
         })
-        .then(res => {
-            if (!res.ok) {
+            .then(res => {
+                if (!res.ok) {
+                    setIsAuthenticated(false)
+                    return null
+                }
+                return res.json()
+            })
+            .then(data => {
+                if (data) setIsAuthenticated(true)
+            })
+            .catch(error => {
                 setIsAuthenticated(false)
-                return null
-            }
-            return res.json()
-        })
-        .then(data => {
-            if (data) setIsAuthenticated(true)
-        })
-        .catch(error => {
-            setIsAuthenticated(false)
-        })
+            })
 
     }, [])
 
     return (
-        <AuthContext.Provider value={{isAuthenticated, setIsAuthenticated}}>
+        <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
             {children}
         </AuthContext.Provider>
     )
