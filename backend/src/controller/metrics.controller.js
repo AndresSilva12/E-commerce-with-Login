7a584d63c674fc0a9fb4f84ec8a606758eeb1248
Andres Silva <andres.silva41202@gmail.com>
2025-09-02 15:27:15 -0300

    feature: implement sales group by products and profits, costs, revenue for each one

--- backend/src/controller/metrics.controller.js
+++ backend/src/controller/metrics.controller.js
@@ -38,13 +38,68 @@ export const getMetrics = async(req, res) => {
             }
         })
 
+        //Traemos todas las unidades vendidas agrupadas por variante
+        const salesByProduct = await prisma.saleItem.groupBy({
+            by: "variantId",
+            _sum: {
+                quantity: true, //cantidad de unidades vendidas
+            },
+            
+            where: {
+                sale: {
+                    date: {
+                        gte: where.date.gte,
+                        lt: where.date.lt
+                    }
+                }
+            }
+        })
+
+        const variantIds = salesByProduct.map(sale => sale.variantId)
+
+        const salesWithInfo = await prisma.saleItem.findMany({
+            where:{
+                sale: {
+                    date: {
+                        gte: where.date.gte,
+                        lt: where.date.lt
+                    }
+                },
+                variantId: {
+                    in: variantIds
+                }
+            },
+            include: {
+                variant: {
+                    include: {
+                        product: true
+                    }
+                }
+            }
+        })
+        
+        const ventasProductos = salesByProduct.map((sale => {
+            const cantidadTotal = sale._sum.quantity
+            const saleInfo = salesWithInfo.filter(s => s.variantId === sale.variantId)
+            const totalVentas = saleInfo.reduce((acc, item) => acc + item.unitPrice * item.quantity, 0)
+            const totalCosto = saleInfo.reduce((acc, item) => acc + item.purchasePrice * item.quantity, 0)
+            return {
+                productName: saleInfo[0].variant.product.name,
+                code: saleInfo[0].variant.code,
+                totalVendido: totalVentas,
+                cantidadVendido: cantidadTotal,
+                totalCosto: totalCosto,
+                gananciaDelProducto: totalVentas - totalCosto
+            }
+        }))
+
         const ingresos = sales.reduce((accumulator, sale) => accumulator + Number(sale.totalPrice), 0)
         const ventasTotales = sales.length
         const costos = Number(sales.flatMap(sale => sale.items.map(item => Number(item.purchasePrice) * item.quantity)).reduce((accumulator, currentPrice) => accumulator + currentPrice, 0).toFixed(2))
         const gananciaNeta = ingresos - costos
         const expenses = await prisma.expenses.findMany({where})
         const totalExpenses = expenses.reduce((accumulator, currentValue) => accumulator + Number(currentValue.amount), 0)
-        return res.json({ingresos: ingresos , ventasTotales: ventasTotales, expenses: expenses, totalExpenses: totalExpenses, costos: costos, gananciaNeta: gananciaNeta})
+        return res.json({ingresos: ingresos , ventasTotales: ventasTotales, expenses: expenses, totalExpenses: totalExpenses, costos: costos, gananciaNeta: gananciaNeta, ventasProductos: ventasProductos})
 
     } catch (error) {
         console.log(error)
