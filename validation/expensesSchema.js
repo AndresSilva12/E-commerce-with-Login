1f571142804bdd3c23940947cb6c8a4ab4be7750
Andres Silva <andres.silva41202@gmail.com>
2025-09-05 19:25:11 -0300

    feature: create zodSchema for validate expenses

---
+++ validation/expensesSchema.js
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const expensesSchema = z.object({
+    name: z
+    .string({
+      required_error: "El nombre es obligatorio",
+      invalid_type_error: "El nombre debe ser una cadena de texto",
+    })
+    .trim()
+    .min(1, "El nombre del gasto es obligatorio")
+    .min(2, "Debe tener almenos 2 caracteres")
+    .regex(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, "Debe incluir solo letras y espacios")
+    .transform((val) => val.toLowerCase().trim()),
+    amount: z.coerce
+    .number()
+    .min(1, "El monto es obligatorio")
+    .gte(1, "El monto debe ser mayor a 0"),
+})
+
+export const updateExpensesSchema = expensesSchema.partial()
