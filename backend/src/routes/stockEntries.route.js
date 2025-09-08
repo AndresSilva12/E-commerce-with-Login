742abe0a5b16f34c01c5a1e4a0a6502b9e7d76ab
Andres Silva <andres.silva41202@gmail.com>
2025-09-08 19:11:27 -0300

    feat: implement deleteEntry function on 'stockEntries.route.js'

--- backend/src/routes/stockEntries.route.js
+++ backend/src/routes/stockEntries.route.js
@@ -1,11 +1,11 @@
 import { Router} from "express"
 import { createStockEntry, getAllStockEntries, deleteEntry } from "../controller/stockEntries.controller.js"
-import { validateStockEntry } from "../middlewares/stockEntriesMiddlewares.js"
+import { validateStockEntry, validateStockEntryExist } from "../middlewares/stockEntriesMiddlewares.js"
 import { authenticate } from "../middlewares/authMiddlewares.js"
 const router = Router()
 
 router.post('/entries', authenticate, validateStockEntry, createStockEntry)
 router.get('/entries', getAllStockEntries)
-router.delete('/entries/:id', deleteEntry)
+router.delete('/entries/:id', validateStockEntryExist,  deleteEntry)
 
 export default router
