73047f31f9212cc584078410c0438dd3e51ae60c
Andres Silva <andres.silva41202@gmail.com>
2025-09-09 12:19:47 -0300

    feat: create 'validateCategoryExist' middleware

--- backend/src/middlewares/categoryMiddleware.js
+++ backend/src/middlewares/categoryMiddleware.js
@@ -25,3 +25,20 @@ export const validateCreateCategory = async (req, res, next) => {
         console.log(error)
     }
 }
+
+export const validateCategoryExist = async (req, res, next) => {
+    try {
+        const id = req.params.id
+        const categoryExist = await prisma.category.findFirst({
+            where: {
+                id: id
+            }
+        })
+        if(!categoryExist) return res.status(400).json({error: "El id no corresponde a ninguna categoría"})
+
+        req.category = categoryExist
+        next()
+    } catch (error) {
+        return res.status(500).json({error: "Error interno del servidor"})
+    }
+}
