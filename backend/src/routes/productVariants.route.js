a2e56a2a7cabe5cdbfa8462211aca262d1e8ad0e
Andres Silva <andres.silva41202@gmail.com>
2025-09-10 09:29:55 -0300

    feat: create endpoint for disableVariant

--- backend/src/routes/productVariants.route.js
+++ backend/src/routes/productVariants.route.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { createVariant, deleteAllVariantsByProduct, deleteVariant, getVariants, getAllVariantsByProduct, getOnlyOneVariant, updateVariant } from '../controller/productVariant.controller.js';
+import { createVariant, deleteAllVariantsByProduct, deleteVariant, getVariants, getAllVariantsByProduct, getOnlyOneVariant, updateVariant, disableVariant } from '../controller/productVariant.controller.js';
 import { validateCreateVariant, validateUpdateVariant, validateVariantExist, validateUniqueCode } from '../middlewares/productVariantMiddlewares.js';
 
 const router = Router()
@@ -19,5 +19,7 @@ router.delete('/variants/id/:id', validateVariantExist, deleteVariant)
 router.delete('/variants/product/:productId', deleteAllVariantsByProduct)
 
 router.put('/variants/:id',validateVariantExist, validateUpdateVariant, updateVariant)
+
+router.patch('/variants/:id/disable', disableVariant)
 
 export default router
