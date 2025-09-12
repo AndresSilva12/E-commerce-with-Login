fa3df94ec81aebd0a169847190df8f2cd301e033
Andres Silva <andres.silva41202@gmail.com>
2025-09-12 19:48:08 -0300

    feat: Improve error handling and responses in useStockEntries

--- frontend/src/hooks/useStockEntries.js
+++ frontend/src/hooks/useStockEntries.js
@@ -1,23 +1,39 @@
 import { toast } from "../utils/notifyToast.js"
 import { useProducts } from "../context/ProductContext"
-import { useState } from "react"
+import { useState, useContext } from "react"
+import { AuthContext } from '../context/AuthContext'
+import {handleAuth} from "../utils/auth.js"
+import { useNavigate } from 'react-router-dom'
+
 
 export function useStockEntries () {
     const [stockEntries, setStockEntries] = useState([])
     const [totalPages, setTotalPages] = useState()
+    const navigate = useNavigate()
     const {updateVariantToProduct} = useProducts()
+    const {setIsAuthenticated} = useContext(AuthContext)
 
     const getAllStockEntries = async(query) => {
-        const res = await fetch(`http://localhost:3000/api/entries?${query}`, {
-            method: 'GET',
-            headers: {
-                "content-type": "application/json"
-            },
-            credentials: "include"
-        })
-        const data = await res.json()
-        setStockEntries(data.allStockEntries)
-        setTotalPages(data.pagination.totalPages)
+        try{
+            const res = await fetch(`http://localhost:3000/api/entries?${query}`, {
+                method: 'GET',
+                headers: {
+                    "content-type": "application/json"
+                },
+                credentials: "include"
+            })
+            const data = await res.json()
+
+            if (!res.ok){
+                handleAuth(data, setIsAuthenticated, navigate)
+                return
+            }
+
+            setStockEntries(data.allStockEntries)
+            setTotalPages(data.pagination.totalPages)
+        } catch (error) {
+            console.log(error.message)
+        }
     }
 
     const createEntry = async(entryData) => {
@@ -31,14 +47,17 @@ export function useStockEntries () {
                 credentials: "include"
             })
             const data = await res.json()
+            
             if (!res.ok){
-                    console.log("Hubo un error durante la creación", data)
-                    return
-                }
+                handleAuth(data, setIsAuthenticated, navigate)
+                return
+            }
+
             setStockEntries((prev) => ([...prev, data]))
             for (const variant of data.updatedVariants){
                 updateVariantToProduct(variant.productId, variant.id, variant)
             }
+            
         }
         catch (error) {
             console.log(error)
@@ -46,16 +65,26 @@ export function useStockEntries () {
     }
 
     const deleteEntry = async(id) => {
-        const res = await fetch(`http://localhost:3000/api/entries/${id}`,{
-            method: 'DELETE',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            credentials: "include"
-        })
-        const data = await res.json()
-        setStockEntries((prev) => (prev.filter(e => e.id !== id)))
-        toast("Entrada eliminada con exito!")
+        try {
+            const res = await fetch(`http://localhost:3000/api/entries/${id}`,{
+                method: 'DELETE',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "include"
+            })
+            const data = await res.json()
+            
+            if (!res.ok){
+                handleAuth(data, setIsAuthenticated, navigate)
+                return
+            }
+    
+            setStockEntries((prev) => (prev.filter(e => e.id !== id)))
+            toast("Entrada eliminada con exito!")
+        } catch (error) {
+            console.log(error.message)
+        }
     }
 
     return {
