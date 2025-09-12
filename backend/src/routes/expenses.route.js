240962c52bf0633b78dcdb2f582b7d35653d749f
Andres Silva <andres.silva41202@gmail.com>
2025-09-12 16:15:07 -0300

    feat/authenticate: add authenticate middleware to expenses  endpoints and include credentials in expenses fetchs

--- backend/src/routes/expenses.route.js
+++ backend/src/routes/expenses.route.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createNewExpense, getAllExpenses, deleteExpense, updateExpense } from "../controller/expenses.controller.js";
 import { validateCreateExpense, validateUpdateExpense, validateExpenseExist } from "../middlewares/expensesMiddlewares.js";
+import { authenticate } from "../middlewares/authMiddlewares.js"
 
 const router = Router()
 
-router.get('/expenses', getAllExpenses)
-router.post('/expenses', validateCreateExpense, createNewExpense)
-router.delete('/expenses/:id', validateExpenseExist, deleteExpense)
-router.put('/expenses/:id', validateExpenseExist, validateUpdateExpense, updateExpense)
+router.get('/expenses', authenticate, getAllExpenses)
+router.post('/expenses', authenticate, validateCreateExpense, createNewExpense)
+router.delete('/expenses/:id', authenticate, validateExpenseExist, deleteExpense)
+router.put('/expenses/:id', authenticate, validateExpenseExist, validateUpdateExpense, updateExpense)
 
 export default router;
--- frontend/src/hooks/useExpenses.js
+++ frontend/src/hooks/useExpenses.js
@@ -3,7 +3,8 @@ export function useExpenses () {
         const res = await fetch('http://localhost:3000/api/expenses',{
             method: 'POST',
             headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(formData)
+            body: JSON.stringify(formData),
+            credentials: "include"
         })
         const data = await res.json()
         console.log(data)
@@ -13,7 +14,8 @@ export function useExpenses () {
         const res = await fetch(`http://localhost:3000/api/expenses/${id}`,{
             method: 'PUT',
             headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(formData)
+            body: JSON.stringify(formData),
+            credentials: "include"
         })
         const data = await res.json()
         console.log(data)
@@ -22,7 +24,8 @@ export function useExpenses () {
     const deleteExpense = async (id) => {
         const res = await fetch(`http://localhost:3000/api/expenses/${id}`,{
             method: 'DELETE',
-            headers: {"Content-Type": "application/json"}
+            headers: {"Content-Type": "application/json"},
+            credentials: "include"
         })
         const data = await res.json()
         console.log(data)
