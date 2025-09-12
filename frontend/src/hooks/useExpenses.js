75bb0840de9ed2f4752f6b53a002834c1ebfce3a
Andres Silva <andres.silva41202@gmail.com>
2025-09-12 16:35:35 -0300

    enhancement: Improve error handling and responses in useExpenses

--- frontend/src/hooks/useExpenses.js
+++ frontend/src/hooks/useExpenses.js
@@ -1,34 +1,70 @@
+import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext"
+import { handleAuth } from "../utils/auth.js"
+import { useContext } from "react"
+
 export function useExpenses () {
+    const navigate = useNavigate()
+    const { setIsAuthenticated} = useContext(AuthContext)
+
     const createExpense = async (formData) => {
-        const res = await fetch('http://localhost:3000/api/expenses',{
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(formData),
-            credentials: "include"
-        })
-        const data = await res.json()
-        console.log(data)
+        try {
+            const res = await fetch('http://localhost:3000/api/expenses',{
+                method: 'POST',
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(formData),
+                credentials: "include"
+            })
+            const data = await res.json()
+
+            if (!res.ok){
+                handleAuth(data, setIsAuthenticated, navigate)
+                return
+            }
+            console.log(data)
+            
+        } catch (error) {
+            console.log(error.message)
+        }
     }
 
     const updateExpense = async (formData, id) => {
-        const res = await fetch(`http://localhost:3000/api/expenses/${id}`,{
-            method: 'PUT',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(formData),
-            credentials: "include"
-        })
-        const data = await res.json()
-        console.log(data)
+        try {
+            const res = await fetch(`http://localhost:3000/api/expenses/${id}`,{
+                method: 'PUT',
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(formData),
+                credentials: "include"
+            })
+            const data = await res.json()
+            
+            if (!res.ok){
+                handleAuth(data, setIsAuthenticated, navigate)
+                return
+            }
+            console.log(data)
+        } catch (error) {
+            console.log(error.message)
+        }
     }
 
     const deleteExpense = async (id) => {
-        const res = await fetch(`http://localhost:3000/api/expenses/${id}`,{
-            method: 'DELETE',
-            headers: {"Content-Type": "application/json"},
-            credentials: "include"
-        })
-        const data = await res.json()
-        console.log(data)
+        try {
+            const res = await fetch(`http://localhost:3000/api/expenses/${id}`,{
+                method: 'DELETE',
+                headers: {"Content-Type": "application/json"},
+                credentials: "include"
+            })
+            const data = await res.json()
+            
+            if (!res.ok){
+                handleAuth(data, setIsAuthenticated, navigate)
+                return
+            }
+            console.log(data)
+        } catch (error) {
+            console.log(error.message)
+        }
     }
 
     return {
