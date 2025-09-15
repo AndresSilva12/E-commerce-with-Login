4256d001c21bd6a23d6eabc370b3c5af8276ed31
Andres Silva <andres.silva41202@gmail.com>
2025-09-15 14:25:08 -0300

    feat/authorize: add authorize middleware to report endpoint

--- backend/src/routes/report.route.js
+++ backend/src/routes/report.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import {createReport} from "../controller/report.controller.js"
-import { authenticate } from "../middlewares/authMiddlewares.js";
+import { authenticate, authorizeRoles } from "../middlewares/authMiddlewares.js";
 
 const router = Router()
 
-router.post("/report", authenticate, createReport)
+router.post("/report", authenticate, authorizeRoles, createReport)
 
 export default router;
