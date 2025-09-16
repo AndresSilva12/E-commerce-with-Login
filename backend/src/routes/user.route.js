9d162787a2c5293b769bc0a056938fce75443fcc
Andres Silva <andres.silva41202@gmail.com>
2025-09-15 21:15:18 -0300

    feat: create endpoints for put and delete my user

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { createUser, getAllUsers, deleteUserSelected, updateUserSelected, loginUser, getCurrentUser, logoutUser, refreshSesion } from "../controller/user.controller.js"
+import { createUser, getAllUsers, deleteUserSelected, updateMyUser, deleteMyUser, loginUser, getCurrentUser, logoutUser, refreshSesion } from "../controller/user.controller.js"
 import { validateCreateUsers, validateUserExist, validateLoginUser, validateUpdateUser } from "../middlewares/usersMiddlewares.js"
 import { authenticate, authorizeRoles } from "../middlewares/authMiddlewares.js"
 
@@ -13,12 +13,14 @@ router.post('/refresh-token', authenticate, refreshSesion)
 
 router.post('/logout', authenticate, logoutUser)
 
-router.get('/me', authenticate, getCurrentUser)
-
 router.get('/users', authenticate, authorizeRoles, getAllUsers)
 
-router.delete('/users/:id', authenticate, validateUserExist, deleteUserSelected)
+router.delete('/users/:id', authenticate, authorizeRoles, validateUserExist, deleteUserSelected)
 
-router.put('/users/:id',authenticate,  validateUserExist, validateUpdateUser, updateUserSelected)
+router.get('/me', authenticate, getCurrentUser)
+
+router.put('/me',authenticate,  validateUserExist, validateUpdateUser, updateMyUser)
+
+router.delete('/me',authenticate,  validateUserExist, deleteMyUser)
 
 export default router
