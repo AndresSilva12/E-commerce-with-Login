a2c62512228bf8b8384fc7e7fb6bcecaad4d086d
Andres Silva <andres.silva41202@gmail.com>
2025-09-26 12:03:44 -0300

    enhancement: improvement validation in expensesSchema and add ErrorMessage component in ExpenseModal

--- frontend/src/components/ExpenseModal.jsx
+++ frontend/src/components/ExpenseModal.jsx
@@ -4,6 +4,7 @@ import { useForm } from "react-hook-form"
 import { useExpenses } from "../hooks/useExpenses.js"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { expensesSchema, updateExpensesSchema } from "../../../validation/expensesSchema.js"
+import ErrorMessage from "./ErrorMessage"
 
 function ExpenseModal({ expenseUpdate, onSubmitExpense }) {
     const { createExpense, updateExpense } = useExpenses()
@@ -33,21 +34,21 @@ function ExpenseModal({ expenseUpdate, onSubmitExpense }) {
                         <Field.Root invalid={!!errors.name}>
                             <Box display="flex" gap="4" width="100%" justifyContent="space-between">
                                 <Field.Label width="50%">Nombre del gasto</Field.Label>
-                                <Field.ErrorText>{errors.name?.message}</Field.ErrorText>
                                 <Box width="50%">
                                     <Input {...register("name")} />
+                                    <ErrorMessage error={errors.name} />
                                 </Box>
                             </Box>
                         </Field.Root>
                         <Field.Root invalid={!!errors.amount}>
                             <Box display="flex" gap="4" width="100%" justifyContent="space-between">
                                 <Field.Label width="50%">Total</Field.Label>
-                                <Field.ErrorText>{errors.amount?.message}</Field.ErrorText>
                                 <Box width="50%">
                                     <NumberInput.Root defaultValue="1">
                                         <NumberInput.Control />
                                         <NumberInput.Input  {...register("amount")} />
                                     </NumberInput.Root>
+                                    <ErrorMessage error={errors.amount} />
                                 </Box>
                             </Box>
                         </Field.Root>
